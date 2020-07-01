396b56275e667eca44a23aae8b4ce48fdcc70873
Justin Martinelli <justinmartinelli1@gmail.com>
2020-06-30 22:56:58 -0400

    Fixed some of the contact page

--- src/contact.js
+++ src/contact.js
@@ -10,10 +10,14 @@ class Contact extends Component {
                 <div className="contact-div">
                     <h1> CONTACT </h1>
                     <Col>
-                        <Image className="icon-pic" src={thumbnailPic} roundedCircle />
-                    </Col>
-                    <Col>
-                        <h1 className="">JUSTIN MARTINELLI</h1>
+                        <div class="col-md-3 contact-info">
+                            <ul class="contact-info">
+                                <li class="phone"><a href="tel:7187576212">(718)7576212</a></li>
+                                <li class="email"><a href="mailto:justinmartinelli1@gmail.com">justinmartinelli1@gmail.com</a></li>
+                                <li class="linkedin"><a href="https://www.linkedin.com/in/justin-martinelli-nyc">LinkedIn</a></li>
+                                <li class="github"><a href="https://github.com/justinm-nyc">GitHub</a></li>
+                            </ul>
+                        </div>
                     </Col>
                 </div>
             </div>
