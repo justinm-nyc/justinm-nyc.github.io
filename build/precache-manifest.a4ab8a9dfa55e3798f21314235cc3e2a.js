0e4413252dada3d8c92b8a702b759533cffecac8
Justin Martinelli <justinmartinelli1@gmail.com>
2020-07-06 11:02:00 -0400

    Version with new dependencies and build folder

---
+++ build/precache-manifest.a4ab8a9dfa55e3798f21314235cc3e2a.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b3b6083ddf59d4200b6d5afcc553d9c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2801bbfbaaea9d149694",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "1a8eaad3e828c7354217",
+    "url": "/static/css/main.430eb468.chunk.css"
+  },
+  {
+    "revision": "2801bbfbaaea9d149694",
+    "url": "/static/js/2.b6b550bc.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.b6b550bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a8eaad3e828c7354217",
+    "url": "/static/js/main.9bb36e4e.chunk.js"
+  },
+  {
+    "revision": "6ddb1850a5ab69742e45",
+    "url": "/static/js/runtime-main.067b7185.js"
+  },
+  {
+    "revision": "cd5e87077d20cb7a86c4238445606345",
+    "url": "/static/media/Justin_Martinelli_Technical_Resume.cd5e8707.pdf"
+  },
+  {
+    "revision": "13bf544c36c2655940cb7a7ab4400c6b",
+    "url": "/static/media/arrow-down-circle.13bf544c.svg"
+  },
+  {
+    "revision": "e456bf017740a2a2f81d6a5132356660",
+    "url": "/static/media/envelope.e456bf01.svg"
+  },
+  {
+    "revision": "5b238434e2862c877f08572b96c0ef7b",
+    "url": "/static/media/iconmonstr-linkedin-3.5b238434.svg"
+  },
+  {
+    "revision": "c6bfef1013d04df163691bbb6f53775f",
+    "url": "/static/media/justin-m-icon-pic.c6bfef10.jpg"
+  },
+  {
+    "revision": "0643c7b0a0f191738c6fb6a6d91ea28d",
+    "url": "/static/media/screenshot1.0643c7b0.png"
+  },
+  {
+    "revision": "972859121c79920f80b8fb1db3dbae08",
+    "url": "/static/media/screenshot1.97285912.png"
+  },
+  {
+    "revision": "e413c35b842b18521af543dd80cf217b",
+    "url": "/static/media/screenshot1.e413c35b.png"
+  },
+  {
+    "revision": "0ade0a1af1dd46b597ed0e690255d22c",
+    "url": "/static/media/screenshot2.0ade0a1a.png"
+  },
+  {
+    "revision": "2824ebabba121e76d8cf1155c6bd9ef9",
+    "url": "/static/media/screenshot2.2824ebab.png"
+  },
+  {
+    "revision": "d55c5d9764eae3ec84ca9beb8a71f414",
+    "url": "/static/media/screenshot2.d55c5d97.png"
+  },
+  {
+    "revision": "40fe5599ad6365bf9d5aa9bb2ff6e6dd",
+    "url": "/static/media/screenshot3.40fe5599.png"
+  },
+  {
+    "revision": "884ab6440a7bc99819018f87329446fe",
+    "url": "/static/media/screenshot3.884ab644.png"
+  },
+  {
+    "revision": "f04c34485e17708a379af0e0e2193c25",
+    "url": "/static/media/screenshot3.f04c3448.png"
+  },
+  {
+    "revision": "4125d935bcc6b4a9c83ccf8e2c6e59cc",
+    "url": "/static/media/screenshot4.4125d935.png"
+  },
+  {
+    "revision": "77f82df360e9eddf5a9e4fae9a60b74a",
+    "url": "/static/media/screenshot4.77f82df3.png"
+  },
+  {
+    "revision": "86247e3bfac101eff77c8e272d1191f0",
+    "url": "/static/media/telephone.86247e3b.svg"
+  }
+]);
